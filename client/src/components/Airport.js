0e6a6ef1abe7b66a9ddb4f87564ae9b3f0fc311a
Claire Nessler <Claire@Claires-MacBook-Air.local>
2019-02-26 20:47:25 -0700

    sort of clean

--- client/src/components/Airport.js
+++ client/src/components/Airport.js
@@ -5,21 +5,19 @@ class Airport extends React.Component {
     render() {
         return (
             <Container>
-                <div className="container">
-                    <Row>
-                        <Col sm="8">
+                <div id="spacing" className="container h-100">
+                    <Row className="row h-100 justify-content-center align-items-center">
+                        <Col sm="10">
                             <h4>Where to stay</h4>
                                 <p>
                                     Our wedding is technically in Keystone, CO. However, it is almost centered between Keystone and Frisco. 
                                     We plan on spending more time in Frisco but you are free to choose wherever you would like to stay.
                                     The location of the wedding is 130 Kinnikinik Rd Keystone, CO to help plan your visit.
                                 </p>
-                                <p>
-                                    We would recommend:
-                                </p>
-                                <li><a href="">Airbnb</a></li>
-                                <a href="">VRBO</a><break></break>
-                                <a href="https://www.keystoneresort.com/">Keystone Resort</a><break></break>
+                                We would recommend:
+                                <ol><a href="https://www.airbnb.com/">Airbnb</a></ol>
+                                <ol><a href="https://www.vrbo.com/">VRBO</a></ol>
+                                <ol><a href="https://www.keystoneresort.com/">Keystone Resort</a></ol>
 
                             <h4>Airport Transportation</h4>
                                 <p>
@@ -27,16 +25,16 @@ class Airport extends React.Component {
                                     We would recommend one of the following shuttles and would discourage using Uber or Lyft (it's Hella expensive to the mountains).
                                 </p>
                                     <h5>Here are a few shuttle options for Frisco or Keystone</h5>
-                                        <a href="https://www.summitexpress.com/scheduled-shuttle-service/denver-airport-to-frisco-co-shuttle/">Summit Express</a>
-                                        <a href="https://www.mountainshuttle.com/shuttle-services/frisco/">Mountain Shuttle</a>
-                                        <a href="https://www.freshtrackstransportation.com/denver-international-airport-to-frisco/">Fresh Tracks Transportation</a>
-                                        <a href="https://www.epicmountainexpress.com/shuttle-rates">Epic Mountain Express</a>
+                                    <ol><a href="https://www.summitexpress.com/scheduled-shuttle-service/denver-airport-to-frisco-co-shuttle/">Summit Express</a></ol>
+                                    <ol><a href="https://www.mountainshuttle.com/shuttle-services/frisco/">Mountain Shuttle</a></ol>
+                                    <ol><a href="https://www.freshtrackstransportation.com/denver-international-airport-to-frisco/">Fresh Tracks Transportation</a></ol>
+                                    <ol><a href="https://www.epicmountainexpress.com/shuttle-rates">Epic Mountain Express</a></ol>
                             <h4>How the hell do I get around?</h4>
                                 <p>
                                     Where there is a will there is a way.
 
                                     There are multiple local busses that run in Frisco and Keystone for FREE. Check them
-                                    out <a href="https://www.summitcountyco.gov/586/Transit-Summit-Stage">here</a>
+                                    out <a href="https://www.summitcountyco.gov/586/Transit-Summit-Stage">here</a>.
                                 </p>
                         </Col>
                     </Row>
--- client/src/components/Deets.js
+++ client/src/components/Deets.js
@@ -1,54 +1,43 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
 
-class Journey extends React.Component {
+class Deets extends React.Component {
     render() {
         return (
-            <Container id="container">
-                <Row>
-                    <Col sm="12">
-                    <h2>The Details</h2>
-                    <h5>Where:</h5><span> 130 Kinnikinik Rd Keyston, CO</span>
-                    <h5>When:</h5><span>1:00PM - you can't dance no mo</span>
-                    <h5>What to wear:</h5><span>It will be outside so maybe ditch the heels if you want</span>
-                    <h5>Food:</h5><span>We'll feed ya</span>
-                    <h5>Drinks:</h5><span>Hope you like IPA's</span>
+            <Container>
+                <div id="spacing" className="container h-100">
+                    <Row className="row h-100 justify-content-center align-items-center">
+                        <Col sm="10">
+                            <h4 id="detail-title">The Details</h4>
+                                <Row id="details">
+                                    <Col sm="1">
+                                        <Row>Where: </Row>
+                                        <Row>When: </Row>
+                                        <Row>Attire: </Row>
+                                    </Col>
+                                    <Col sm="5">
+                                        <Row>130 Kinnikinik Rd</Row>
+                                        <Row>1:00PM - you can't dance no mo</Row>
+                                        <Row>It will be outside so maybe ditch the heels if you want</Row>
+                                    </Col>
+                                </Row>
 
-                    <h2>How to get there</h2>
-                    <p>
-                        There are a number of cab services you can book in advance. We recommend this route. However, if you
-                        want to drive there is parking at the local club house down the road. It is about a five minute walk
-                        from there to the house. We will have signs to assist any drivers.
-                    </p>
-                    </Col>
-                </Row>
-                    <Row>
-                        <Col sm="12" md={{ size: 4, offset: 4 }}>
-                            <h3 style={{paddingTop:"2rem", paddingBottom:"1rem"}}>Things we know</h3>
+                            <h4 id="padding-top">How to get there</h4>
+                                <p>
+                                    There are a number of cab services you can book in advance. We recommend this route. However, if you
+                                    want to drive there is parking at the local club house down the road. It is about a five minute walk
+                                    from there to the house. We will have signs to assist any drivers.
+                                </p>
+                                <ol><a href="https://www.summitcove.com/taxi-services-summit-county/">Summit Cove Taxi</a></ol>
+                                <ol><a href="http://453taxi.com/areas-we-serve/">453 Taxi</a></ol>
+                                <ol><a href="http://www.jakesmountainshuttle.com/cant-find-a-taxi-or-uber.html">Jake's Mountain Shuttle</a></ol>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col sm="12" md={{ size: 4, offset: 4 }}>
-                    <ListGroup>
-                        <ListGroupItem header="">It will be in Keystone, Colorado</ListGroupItem>
-                        <ListGroupItem header="">
-                            It's on September 13, 2019.
-                        </ListGroupItem>
-                        <ListGroupItem header="">
-                            Should start early and go late
-                        </ListGroupItem>
-                    </ListGroup>
-                    </Col>
-                    </Row>
-                    <Row>
-                        <Col sm="12" md={{ size: 4, offset: 4 }}>
-                            <h3 style={{paddingTop:"2rem"}}>More details to come!</h3>
-                        </Col>
-                    </Row>
+                </div>
             </Container>
 
         )
     }
 }
 
-export default Journey;
+export default Deets;
