b24e9ca85860f28a1e31a3a273045d46014dcfa4
Claire Nessler <Claire@Claires-MacBook-Air.local>
2019-05-23 08:05:16 -0600

    time changes

--- client/src/components/Deets.js
+++ client/src/components/Deets.js
@@ -15,7 +15,7 @@ class Deets extends React.Component {
                                 <Row id="details">
                                     <Col sm="12">
                                         <Row><b>Where:</b> 130 Kinnikinik Rd</Row>
-                                        <Row><b>When:</b> 1:00PM - you can't dance no mo</Row>
+                                        <Row><b>When:</b> late afternoon - late (exact time TBD)</Row>
                                         <Row><b>Who:</b> Everyone! Kids are welcome at all events</Row>
                                         <Row><b>Attire:</b> Mostly outside so chic but practical</Row>
                                     </Col>
@@ -31,7 +31,7 @@ class Deets extends React.Component {
                         <p class="card-text">
                             <ul>
                                 <li>
-                                    Hike on the Colorado Trail at 9:30AM
+                                    Hike close to town (exact hike TBD)
                                 </li>
                                 <li>
                                     After hike beers at Highside Brewery ~12:00PM
@@ -55,9 +55,9 @@ class Deets extends React.Component {
                     <div class="col-sm-12 mb-3 mt-3">
                     <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Sunday the 15th: Chill</h5>
+                        <h5 class="card-title">Sunday the 15th: BBQ & Chill</h5>
                         <p class="card-text">
-                            We will be hanging out at the house. Come over, grab some food, relax in the hot tub,
+                            We will be hanging out at the house. Come over to hang or before your head out, grab some food, relax in the hot tub,
                             drink some drinks. This is all about R&R with friends and family. The more the merrier.
                         </p>
                     </div>
