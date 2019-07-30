54ee1ae255b25ca2505d9ca3ce2f2e6bad979a94
Claire Nessler <Claire@Claires-MacBook-Air.local>
2019-07-29 18:50:56 -0600

    details

--- client/src/components/Deets.js
+++ client/src/components/Deets.js
@@ -14,10 +14,10 @@ class Deets extends React.Component {
                             <p class="card-text">
                                 <Row id="details">
                                     <Col sm="12">
-                                        <Row><b>Where:</b> 130 Kinnikinik Rd</Row>
-                                        <Row><b>When:</b> late afternoon - late (exact time TBD)</Row>
-                                        <Row><b>Who:</b> Everyone! Kids are welcome at all events</Row>
-                                        <Row><b>Attire:</b> Mostly outside so chic but practical</Row>
+                                        <Row><b>Where: &nbsp;</b> <a href='https://www.google.com/maps/place/130+Kinnikinnik+Rd,+Dillon,+CO+80435/@39.5904673,-105.9951747,17z/data=!3m1!4b1!4m5!3m4!1s0x876a59ed51d73b03:0x2f6c3baf456bbfe8!8m2!3d39.5904673!4d-105.992986' target="_blank">130 Kinnikinik Rd, Keystone CO</a></Row>
+                                        <Row><b>When: &nbsp;</b> 4PM - 11:00</Row>
+                                        <Row><b>Who: &nbsp;</b> Everyone! Kids are welcome at all events</Row>
+                                        <Row><b>Attire: &nbsp;</b> Mostly outside so chic but practical</Row>
                                     </Col>
                                 </Row>
                             </p>
@@ -31,10 +31,17 @@ class Deets extends React.Component {
                         <p class="card-text">
                             <ul>
                                 <li>
-                                    Hike close to town (exact hike TBD)
+                                    Hike Rainbow Lakes Trail outside of Frisco
+                                    <ul>
+                                        <li><a href="https://www.google.com/maps/place/Rainbow+Lake+Trailhead/@39.5728056,-106.0996389,16z/data=!4m18!1m12!4m11!1m6!1m2!1s0x876a5e9750296981:0xd8548ba4397f4cbf!2sHighSide+Brewing,+Main+Street,+Frisco,+CO!2m2!1d-106.0928118!2d39.5763608!1m3!2m2!1d-106.09869!2d39.56886!3m4!1s0x876a5e8d85e02eed:0x742926aa06e49970!8m2!3d39.5689514!4d-106.0997933" target="_blank"
+                                                >trailhead </a> </li>
+                                        <li><a href="https://www.alltrails.com/trail/us/colorado/rainbow-lake" target="_blank"
+                                                >details </a> </li>
+                                    </ul>
                                 </li>
                                 <li>
-                                    After hike beers at Highside Brewery ~12:00PM
+                                    After hike beers at <a href="https://www.google.com/maps/place/HighSide+Brewing/@39.5763608,-106.0928118,15z/data=!4m5!3m4!1s0x0:0xd8548ba4397f4cbf!8m2!3d39.5763608!4d-106.0928118" target="_blank"
+                                                >Highside Brewery </a>  1:30PM
                                 </li>
                                 <li>
                                     Lake Dillon is across the street with a playground and/or boat rentals for those
@@ -44,7 +51,9 @@ class Deets extends React.Component {
                                     Walk Main St. to check out the town
                                 </li>
                             </ul>
-                            More to come on the exact hiking trail. Also, please note this is not a hosted event
+                            
+                                        
+                            Please note this is not a hosted event
                             and everyone is responsible for their own tab.
                         </p>
                     </div>
